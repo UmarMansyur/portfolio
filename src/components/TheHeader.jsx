a0870f98ca502fcdaa68f231c31bc2c71bacb316
Muhammad Umar Mansyur <umar.ovie@gmail.com>
2024-08-13 19:43:39 +0700

    push

---
+++ src/components/TheHeader.jsx
@@ -0,0 +1,18 @@
+export default function TheHeader() {
+  return (
+    <nav className="bg-transparent">
+     <div className="flex justify-between">
+      <div>
+        
+      </div>
+      <div className="flex space-x-4 font-sans">
+        <a href="">About</a>
+        <a href="">Skill</a>
+        <a href="">Portofolio</a>
+        <a href="">Testimoni</a>
+      </div>
+      <div></div>
+     </div>
+    </nav>
+  );
+}
